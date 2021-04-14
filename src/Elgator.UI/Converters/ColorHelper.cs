c5ba1003e9526e0d1d1c9655bc59027a7eb572ad
Genadij Trimailov <genadij.trimailov@solita.fi>
2021-04-14 02:02:03 +0200

    cleanup

---
+++ src/Elgator.UI/Converters/ColorHelper.cs
@@ -0,0 +1,57 @@
+﻿using Avalonia.Media;
+
+namespace Elgator.UI.Converters
+{
+    public static class ColorHelper
+    {
+        // based on Elgato's ControlCenter.Views.GradientStopCollectionExtensions
+        public static Color GetRelativeColor(Color startColor, Color endColor, double startValue, double endValue, double value)
+        {
+            double numA = (value - startValue) * (endColor.A - startColor.A) / (endValue - startValue);
+            Color c = startColor;
+            double a = c.A;
+            int num2 = (byte)(numA + a);
+            var A = (byte)num2;
+
+            double numR = value - startValue;
+            c = endColor;
+            int r1 = c.R;
+            c = startColor;
+            int r2 = c.R;
+            double num4 = r1 - r2;
+            double ratioR = numR * num4 / (endValue - startValue);
+            c = startColor;
+            double r3 = c.R;
+            int num6 = (byte)(ratioR + r3);
+            var R = (byte)num6;
+
+            double num7 = value - startValue;
+            c = endColor;
+            int g1 = c.G;
+            c = startColor;
+            int g2 = c.G;
+            double num8 = g1 - g2;
+            double ratioG = num7 * num8 / (endValue - startValue);
+            c = startColor;
+            double g3 = c.G;
+            int numG = (byte)(ratioG + g3);
+            var G = (byte)numG;
+
+            double num11 = value - startValue;
+            c = endColor;
+            int b1 = c.B;
+            c = startColor;
+            int b2 = c.B;
+            double bDiff = b1 - b2;
+            double ratioB = num11 * bDiff / (endValue - startValue);
+            c = startColor;
+            double b3 = c.B;
+            int numB = (byte)(ratioB + b3);
+            var B = (byte)numB;
+
+            var color = Color.FromArgb(A, R, G, B);
+
+            return color;
+        }
+    }
+}
--- src/Elgator.UI/Converters/ThumbColorConverter.cs
+++ src/Elgator.UI/Converters/ThumbColorConverter.cs
@@ -7,18 +7,18 @@ using System.Linq;
 
 namespace Elgator.UI.Converters
 {
-    public class ThumbColorConverter : IMultiValueConverter
+    public partial class ThumbColorConverter : IMultiValueConverter
     {
         public object Convert(IList<object> values, Type targetType, object parameter, CultureInfo culture)
         {
-            if(values == null || values.Count() != 4)
+            if (values == null || values.Count() != 4)
             {
                 return new SolidColorBrush(Colors.Red);
             }
 
             var brush = values[0] as LinearGradientBrush;
 
-            if(brush == null)
+            if (brush == null)
             {
                 return new SolidColorBrush(Colors.Red);
             }
@@ -29,58 +29,9 @@ namespace Elgator.UI.Converters
             var endValue = (double)values[2];
             var value = (double)values[3];
 
-            var color = GetRelativeColor(startColor, endColor, startValue, endValue, value);
+            var color = ColorHelper.GetRelativeColor(startColor, endColor, startValue, endValue, value);
 
             return new SolidColorBrush(color);
-        }
-
-        public Color GetRelativeColor(Color startColor, Color endColor, double startValue, double endValue, double value)
-        {
-            double numA = (value - startValue) * (endColor.A - startColor.A) / (endValue - startValue);
-            Color c = startColor;
-            double a = c.A;
-            int num2 = (byte)(numA + a);
-            var A = (byte)num2;
-
-            double numR = value - startValue;
-            c = endColor;
-            int r1 = c.R;
-            c = startColor;
-            int r2 = c.R;
-            double num4 = r1 - r2;
-            double ratioR = numR * num4 / (endValue - startValue);
-            c = startColor;
-            double r3 = c.R;
-            int num6 = (byte)(ratioR + r3);
-            var R = (byte)num6;
-
-            double num7 = value - startValue;
-            c = endColor;
-            int g1 = c.G;
-            c = startColor;
-            int g2 = c.G;
-            double num8 = g1 - g2;
-            double ratioG = num7 * num8 / (endValue - startValue);
-            c = startColor;
-            double g3 = c.G;
-            int numG = (byte)(ratioG + g3);
-            var G = (byte)numG;
-
-            double num11 = value - startValue;
-            c = endColor;
-            int b1 = c.B;
-            c = startColor;
-            int b2 = c.B;
-            double bDiff = b1 - b2;
-            double ratioB = num11 * bDiff / (endValue - startValue);
-            c = startColor;
-            double b3 = c.B;
-            int numB = (byte)(ratioB + b3);
-            var B = (byte)numB;
-
-            var color = Color.FromArgb(A, R, G, B);
-
-            return color;
         }
     }
 }
